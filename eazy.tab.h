a8fcfcb4cb126c4d01c2bf2f2eec0ad0142043eb
marinaacastroo <marinaacastroo24@gmail.com>
2025-04-26 12:36:20 +0200

    feat: implement new grammar rules and update parser structure in eazy.y and eazy.l

---
+++ eazy.tab.h
@@ -0,0 +1,221 @@
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     ABSTRACTO = 258,
+     AND = 259,
+     ASIG = 260,
+     AND_ASIG = 261,
+     CADA = 262,
+     CADENA = 263,
+     CARACTER = 264,
+     CLASE = 265,
+     COMO = 266,
+     CONSTANTES = 267,
+     CONSTRUCTOR = 268,
+     CONTINUAR = 269,
+     CTC_CADENA = 270,
+     CTC_CARACTER = 271,
+     CTC_ENTERA = 272,
+     CTC_REAL = 273,
+     DE = 274,
+     DEFECTO = 275,
+     DESTRUCTOR = 276,
+     DEVOLVER = 277,
+     DIV_ASIG = 278,
+     EJECUTA = 279,
+     ENCAMBIO = 280,
+     ENTERO = 281,
+     ENUMERACION = 282,
+     EQ = 283,
+     EN = 284,
+     ES = 285,
+     ESCAPE = 286,
+     ESPECIFICO = 287,
+     ESTRUCTURA = 288,
+     ETIQUETA = 289,
+     EXCEPCION = 290,
+     FD_ASIG = 291,
+     FI_ASIG = 292,
+     FICHERO = 293,
+     FIN = 294,
+     FINAL = 295,
+     FLECHA_DCHA = 296,
+     FLECHA_IZDA = 297,
+     FUNCION = 298,
+     GENERICO = 299,
+     HACER = 300,
+     HASH = 301,
+     GE = 302,
+     IDENTIFICADOR = 303,
+     IMPORTAR = 304,
+     INDIRECCION = 305,
+     LANZA = 306,
+     LE = 307,
+     MIENTRAS = 308,
+     MOD = 309,
+     MOD_ASIG = 310,
+     MULT_ASIG = 311,
+     NADA = 312,
+     NEQ = 313,
+     OR = 314,
+     OTRA = 315,
+     OR_ASIG = 316,
+     PARA = 317,
+     POT_ASIG = 318,
+     POTENCIA = 319,
+     PRINCIPIO = 320,
+     PRIVADO = 321,
+     PROGRAMA = 322,
+     PROTEGIDO = 323,
+     PTOS = 324,
+     PUBLICO = 325,
+     REAL = 326,
+     REF = 327,
+     RESTA_ASIG = 328,
+     SALTAR = 329,
+     SI = 330,
+     SINO = 331,
+     SUMA_ASIG = 332,
+     TAMANO = 333,
+     TABLA = 334,
+     TIPOS = 335,
+     ULTIMA = 336,
+     UNION = 337,
+     VARIABLES = 338,
+     XOR_ASIG = 339
+   };
+#endif
+/* Tokens.  */
+#define ABSTRACTO 258
+#define AND 259
+#define ASIG 260
+#define AND_ASIG 261
+#define CADA 262
+#define CADENA 263
+#define CARACTER 264
+#define CLASE 265
+#define COMO 266
+#define CONSTANTES 267
+#define CONSTRUCTOR 268
+#define CONTINUAR 269
+#define CTC_CADENA 270
+#define CTC_CARACTER 271
+#define CTC_ENTERA 272
+#define CTC_REAL 273
+#define DE 274
+#define DEFECTO 275
+#define DESTRUCTOR 276
+#define DEVOLVER 277
+#define DIV_ASIG 278
+#define EJECUTA 279
+#define ENCAMBIO 280
+#define ENTERO 281
+#define ENUMERACION 282
+#define EQ 283
+#define EN 284
+#define ES 285
+#define ESCAPE 286
+#define ESPECIFICO 287
+#define ESTRUCTURA 288
+#define ETIQUETA 289
+#define EXCEPCION 290
+#define FD_ASIG 291
+#define FI_ASIG 292
+#define FICHERO 293
+#define FIN 294
+#define FINAL 295
+#define FLECHA_DCHA 296
+#define FLECHA_IZDA 297
+#define FUNCION 298
+#define GENERICO 299
+#define HACER 300
+#define HASH 301
+#define GE 302
+#define IDENTIFICADOR 303
+#define IMPORTAR 304
+#define INDIRECCION 305
+#define LANZA 306
+#define LE 307
+#define MIENTRAS 308
+#define MOD 309
+#define MOD_ASIG 310
+#define MULT_ASIG 311
+#define NADA 312
+#define NEQ 313
+#define OR 314
+#define OTRA 315
+#define OR_ASIG 316
+#define PARA 317
+#define POT_ASIG 318
+#define POTENCIA 319
+#define PRINCIPIO 320
+#define PRIVADO 321
+#define PROGRAMA 322
+#define PROTEGIDO 323
+#define PTOS 324
+#define PUBLICO 325
+#define REAL 326
+#define REF 327
+#define RESTA_ASIG 328
+#define SALTAR 329
+#define SI 330
+#define SINO 331
+#define SUMA_ASIG 332
+#define TAMANO 333
+#define TABLA 334
+#define TIPOS 335
+#define ULTIMA 336
+#define UNION 337
+#define VARIABLES 338
+#define XOR_ASIG 339
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
+extern YYSTYPE yylval;
+
